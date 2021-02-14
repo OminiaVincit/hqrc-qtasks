73a41008dfb97f1787f21ac54296f27fc40a9e99
ripper <k09tranhoan@gmail.com>
2021-02-15 01:21:26 +0900

    modify script for mc

--- nonlinear/runscripts-exp/run_mc_qtasks_ripper.sh
+++ nonlinear/runscripts-exp/run_mc_qtasks_ripper.sh
@@ -6,7 +6,6 @@ EXE=../source/mc_quanrc.py
 SAVE=/home/zoro/Workspace/data/hqrc-qtasks/quan_capacity
 DYNAMIC=ion_trap
 
-NSPINS=5
 NEV=1
 NPROC=126
 NTRIALS=5
@@ -16,18 +15,21 @@ TMAX=25.0
 NTAUS=25
 
 MIND=0
-MAXD=250
+MAXD=50
 BUFFER=1000
 TRAINLEN=3000
 VALEN=1000
 
-for V in 1
+for NSPINS in 5 6 4 3 2
+do
+for V in 5
 do
 for alpha in 1.0
 do
 for bc in 2.0
 do
 python $EXE --alpha $alpha --bcoef $bc --rho 1 --ntrials $NTRIALS --savedir $SAVE --spins $NSPINS --envs $NEV --nproc $NPROC --mind $MIND --maxd $MAXD --tmin $TMIN --tmax $TMAX --ntaus $NTAUS --virtuals $V --dynamic $DYNAMIC --buffer $BUFFER --trainlen $TRAINLEN --vallen $VALEN 
+done
 done
 done
 done
