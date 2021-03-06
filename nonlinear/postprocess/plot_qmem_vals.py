2235ec33d4cdcbfcfd5a40374d59f6e33395d2b5
ripper <k09tranhoan@gmail.com>
2021-03-06 23:07:23 +0900

    Update qmem plot

--- nonlinear/postprocess/plot_qmem_vals.py
+++ nonlinear/postprocess/plot_qmem_vals.py
@@ -36,17 +36,17 @@ if __name__  == '__main__':
     parser.add_argument('--ymax', type=float, default='1.0')
     parser.add_argument('--tmin', type=float, default=0.0, help='Minimum of tauB')
     parser.add_argument('--tmax', type=float, default=25.0, help='Maximum of tauB')
-    parser.add_argument('--ntaus', type=int, default=25, help='Number of tausB')
+    parser.add_argument('--ntaus', type=int, default=125, help='Number of tausB')
     parser.add_argument('--alpha', type=float, default=1.0)
-    parser.add_argument('--bcoef', type=float, default=2.0)
+    parser.add_argument('--bcoef', type=float, default=1.0)
     parser.add_argument('--thres', type=float, default=1e-2)
     parser.add_argument('--V', type=int, default=1)
     parser.add_argument('--width', type=float, default=1.0)
-    parser.add_argument('--nspins', type=int, default=5)
-    parser.add_argument('--nenvs', type=int, default=1)
+    parser.add_argument('--nspins', type=int, default=6)
+    parser.add_argument('--nenvs', type=int, default=2)
     parser.add_argument('--prefix', type=str, default='quanrc_ion_trap_nspins')
-    parser.add_argument('--posfix', type=str, default='len_1000_3000_100_trials_5')
-    
+    parser.add_argument('--posfix', type=str, default='len_1000_3000_100_dmax_20')
+    parser.add_argument('--ntrials', type=int, default=10)
     args = parser.parse_args()
     print(args)
 
@@ -56,9 +56,11 @@ if __name__  == '__main__':
     ymin, ymax = args.ymin, args.ymax
     tmin, tmax, ntaus = args.tmin, args.tmax, args.ntaus
     alpha, bc, width = args.alpha, args.bcoef, args.width
+    ntrials = args.ntrials
     tauBs = list(np.linspace(tmin, tmax, ntaus + 1))
     tauBs = tauBs[1:]
-
+    binfolder = os.path.join(folder, 'binary')
+    
     if alpha == 0.0:
         # variable = a
         vals = np.arange(0.1, 2.1, 0.1)
@@ -79,11 +81,11 @@ if __name__  == '__main__':
         vals = tauBs
         vlabel = '$\\tau B$'
         #xticks = range(0, ntaus, 5)
-        xticks = np.linspace(0, 5, 26)
+        xticks = np.linspace(tmin, tmax, 26)
         xticklabels = ['{:.1f}'.format(t) for t in xticks]
-        xticks2 = np.arange(0.0, 25.1, 5.0)
     cmap = plt.get_cmap("twilight")
-    fig, axs = plt.subplots(3, 1, figsize=(20, 12), gridspec_kw={'height_ratios': [3, 2, 2]})
+    fig, axs = plt.subplots(1, 1, figsize=(20, 6), squeeze=False)
+    axs = axs.ravel()
     ax = axs[0]
     #plt.style.use('seaborn-colorblind')
     plt.rc('font', family='serif')
@@ -91,33 +93,31 @@ if __name__  == '__main__':
     plt.rcParams['font.size']=16
 
     ntitle = '{}_a_{}_bc_{}_{}_thres_{}'.format(prefix, alpha, bc, posfix, args.thres)
-    memarr, ts, mcs = [], [], []
+    memarr, ts = [], []
     for val in vals:
         if alpha == 0.0:
-            valbase = '{}_a_{:.2f}_bc_{:.1f}_tauB_{:.3f}_{}.txt'.format(prefix, val, bc, tauBs[0], posfix)
-            if os.path.isfile(os.path.join(folder, valbase)) == False:
-                valbase = '{}_a_{:.1f}_bc_{:.1f}_tauB_{:.3f}_{}.txt'.format(prefix, val, bc, tauBs[0], posfix)
+            valbase = '{}_a_{:.3f}_bc_{:.3f}_tauB_{:.3f}_{}'.format(prefix, val, bc, tauBs[0], posfix)
         elif bc == 0.0:
-            valbase = '{}_a_{:.1f}_bc_{:.2f}_tauB_{:.3f}_{}.txt'.format(prefix, alpha, val, tauBs[0], posfix)
-            if os.path.isfile(os.path.join(folder, valbase)) == False:
-                valbase = '{}_a_{:.1f}_bc_{:.1f}_tauB_{:.3f}_{}.txt'.format(prefix, alpha, val, tauBs[0], posfix)
+            valbase = '{}_a_{:.3f}_bc_{:.3f}_tauB_{:.3f}_{}'.format(prefix, alpha, val, tauBs[0], posfix)
         else:
-            valbase = '{}_a_{:.1f}_bc_{:.1f}_tauB_{:.3f}_{}.txt'.format(prefix, alpha, bc, val, posfix)
-        memfile = os.path.join(folder, valbase)
-        if os.path.isfile(memfile) == False:
-            print('Not found {}'.format(memfile))
-            continue
-        arr = np.loadtxt(memfile)
-        print('read {} with shape'.format(memfile), arr.shape)
-        loc_arr = arr[:, 1]
-        memarr.append(loc_arr)
-        # loc_arr = loc_arr - np.min(loc_arr)
-        loc_arr[loc_arr < args.thres] = 0.0
-        mcs.append(np.sum(loc_arr))
+            valbase = '{}_a_{:.3f}_bc_{:.3f}_tauB_{:.3f}_{}'.format(prefix, alpha, bc, val, posfix)
+        loc_arr = []
+        for n in range(ntrials):
+            memfile = os.path.join(binfolder, '{}_trial_{}.npy'.format(valbase, n))
+            if os.path.isfile(memfile) == False:
+                print('Not found {}'.format(memfile))
+                continue
+            arr = np.load(memfile)
+            print('read {} with shape'.format(memfile), arr.shape)
+            loc_arr.append(arr[:, 1])
+        loc_arr = np.array(loc_arr)
+        avg_loc_arr = np.mean(loc_arr, axis=0)
+        print('avg shape', avg_loc_arr.shape)
+        memarr.append(avg_loc_arr)
         ts.append(val)
     memarr = np.array(memarr).T
-    ymin, ymax = np.min(memarr), np.max(memarr)
-    #im = plotContour(fig, ax, memarr.T, '{}'.format(ntitle), 16, ymin, ymax, cmap)
+    # ymin, ymax = np.min(memarr), np.max(memarr)
+    # im = plotContour(fig, ax, memarr.T, '{}'.format(ntitle), 16, ymin, ymax, cmap)
     ax.bar(ts, memarr[0], width=width, color=d_colors[0], edgecolor='k', label='d=0')
     for i in range(1, len(d_colors)):
         bt = memarr[:i].reshape(i, -1)
@@ -133,34 +133,18 @@ if __name__  == '__main__':
     #ax.set_yscale('log')
     #ax.set_ylim([5*10**(-2), 10**0])
     
-    ax.set_xticks(xticks)
-    ax.set_xticklabels(labels=xticklabels)
     #ax.set_yticklabels(labels='')
     #ax.set_title('{}'.format(ntitle), fontsize=12)
     #ax.grid(True, which="both", ls="-", color='0.65')
     #ax.legend()
 
-    for i in range(1, 3):
-        if i == 2:
-            # normalize mcs
-            mcs = mcs / np.max(mcs)
-            lb = 'NQMC'
-        else:
-            lb = 'QMC'
-        ax2 = axs[i]
-        ax2.set_ylabel(lb, fontsize=24)
-        #ax2.set_xlabel(vlabel, fontsize=24)
-        #ax2.barh(ts, mcs, height=1.0, edgecolor='k', alpha=0.8)
-        ax2.plot(ts, mcs, alpha=0.8, marker='o', markeredgecolor='k', \
-            markersize=10, linewidth=3, markerfacecolor=plu.VERMILLION)
-        ax2.set_xlim(vals[0], vals[-1])
-        ax2.set_xticks(xticks2)
     
-
     for bx in axs:
-        bx.tick_params(axis='both', which='major', labelsize=16)
-        bx.tick_params(axis='both', which='minor', labelsize=12)
-        bx.tick_params('both', length=10, width=1.0, which='major')
+        bx.set_xticks(xticks)
+        bx.set_xticklabels(labels=xticklabels)
+        #bx.tick_params(axis='both', which='major', labelsize=16)
+        #bx.tick_params(axis='both', which='minor', labelsize=12)
+        bx.tick_params('both', length=10, width=1.0, which='major', labelsize=20)
 
     fig_folder = os.path.join(folder, 'figs')
     if os.path.isdir(fig_folder) == False:
